efbd133a6574991a20209e05bf9ff8149a4bacd3
ferdgl <desarrollador19@tecsersas.com>
2025-02-04 11:41:15 -0500

    [Add] - Component diffusion

---
+++ src/app/modules/diffusion/diffusion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diffusion',
+  standalone: true,
+  imports: [],
+  templateUrl: './diffusion.component.html',
+  styleUrl: './diffusion.component.scss'
+})
+export class DiffusionComponent {
+
+}
